c93e6e2c33bc8efe048e4fdfc858fbe1d5221aed
manthan-lunagariya <manthanlunagariya13@hotmail.com>
2019-03-26 16:39:07 +0530

    adding files

---
+++ einfochips.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"DS.h"
+
+
+
+int main()
+{
+	add_emp();
+	add_emp();
+	add_emp();
+	printf("\n");
+	print_emp();
+	printf("\n");
+	delet_emp();
+	print_emp();
+}
+
